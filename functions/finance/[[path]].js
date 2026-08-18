17286b018304445d17255730b966d7581c97c9b8
rokotove26-png <rokotove26@gmail.com>
2026-08-18 11:59:25 +0300

    Hotfix: route finance OAuth consent through bound origin policy

--- functions/finance/[[path]].js
+++ functions/finance/[[path]].js
@@ -1,2 +1,2 @@
-import {proxyRoleRequest} from '../_mcp_transport.js';
-export async function onRequest(context){return proxyRoleRequest(context,'finance');}
+import {proxyBoundRoleRequest} from '../_mcp_role_entry.js';
+export async function onRequest(context){return proxyBoundRoleRequest(context,'finance');}
