7e225e9cc646e3aadcd618387588a7ced59ea646
rokotove26-png <rokotove26@gmail.com>
2026-08-22 01:37:46 +0300

    feat: serve owner acceptance UI runtime

---
+++ functions/portal/owner-acceptance-ui.js
@@ -0,0 +1,9 @@
+import c0 from './owner-ui-chunks/chunk0.js';
+import c1 from './owner-ui-chunks/chunk1.js';
+import c2 from './owner-ui-chunks/chunk2.js';
+import c3 from './owner-ui-chunks/chunk3.js';
+import c4 from './owner-ui-chunks/chunk4.js';
+import c5 from './owner-ui-chunks/chunk5.js';
+import c6 from './owner-ui-chunks/chunk6.js';
+const SCRIPT=[c0,c1,c2,c3,c4,c5,c6].join('');
+export async function onRequest(){return new Response(SCRIPT,{status:200,headers:{'content-type':'application/javascript; charset=utf-8','cache-control':'no-store','x-content-type-options':'nosniff'}})}
