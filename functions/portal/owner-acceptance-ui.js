af2fc1aedc05f1e5a02628498a5b5ab113e46399
rokotove26-png <rokotove26@gmail.com>
2026-08-22 16:48:55 +0300

    feat: wire visual redesign runtime into owner portals

--- functions/portal/owner-acceptance-ui.js
+++ functions/portal/owner-acceptance-ui.js
@@ -5,5 +5,6 @@ import c3 from './owner-ui-chunks/chunk3.js';
 import c4 from './owner-ui-chunks/chunk4.js';
 import c5 from './owner-ui-chunks/chunk5.js';
 import c6 from './owner-ui-chunks/chunk6.js';
-const SCRIPT=[c0,c1,c2,c3,c4,c5,c6].join('');
+import c7 from './owner-ui-chunks/chunk7.js';
+const SCRIPT=[c0,c1,c2,c3,c4,c5,c6,c7].join('');
 export async function onRequest(){return new Response(SCRIPT,{status:200,headers:{'content-type':'application/javascript; charset=utf-8','cache-control':'no-store','x-content-type-options':'nosniff'}})}
