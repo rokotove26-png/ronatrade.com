6b7f96cb8f1316e4968e3e204e56fbdd5492310a
rokotove26-png <rokotove26@gmail.com>
2026-08-22 17:09:44 +0300

    feat: wire final Admin visual polish

--- functions/portal/owner-acceptance-ui.js
+++ functions/portal/owner-acceptance-ui.js
@@ -7,5 +7,6 @@ import c5 from './owner-ui-chunks/chunk5.js';
 import c6 from './owner-ui-chunks/chunk6.js';
 import c7 from './owner-ui-chunks/chunk7.js';
 import c8 from './owner-ui-chunks/chunk8.js';
-const SCRIPT=[c0,c1,c2,c3,c4,c5,c6,c7,c8].join('');
+import c9 from './owner-ui-chunks/chunk9.js';
+const SCRIPT=[c0,c1,c2,c3,c4,c5,c6,c7,c8,c9].join('');
 export async function onRequest(){return new Response(SCRIPT,{status:200,headers:{'content-type':'application/javascript; charset=utf-8','cache-control':'no-store','x-content-type-options':'nosniff'}})}
