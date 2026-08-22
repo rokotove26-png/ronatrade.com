8a413432b7f45de86769229a5a18d4b70b3458a4
rokotove26-png <rokotove26@gmail.com>
2026-08-22 18:29:05 +0300

    fix: include original background restoration layer

--- functions/portal/owner-acceptance-ui.js
+++ functions/portal/owner-acceptance-ui.js
@@ -9,5 +9,6 @@ import c7 from './owner-ui-chunks/chunk7.js';
 import c8 from './owner-ui-chunks/chunk8.js';
 import c9 from './owner-ui-chunks/chunk9.js';
 import c10 from './owner-ui-chunks/chunk10.js';
-const SCRIPT=[c0,c1,c2,c3,c4,c5,c6,c7,c8,c9,c10].join('');
+import c11 from './owner-ui-chunks/chunk11.js';
+const SCRIPT=[c0,c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11].join('');
 export async function onRequest(){return new Response(SCRIPT,{status:200,headers:{'content-type':'application/javascript; charset=utf-8','cache-control':'no-store','x-content-type-options':'nosniff'}})}
