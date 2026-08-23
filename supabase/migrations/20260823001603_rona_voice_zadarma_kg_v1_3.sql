76bcfbcb1d0ba768c8dd54f72948c93b201e2465
rokotove26-png <rokotove26@gmail.com>
2026-08-23 03:16:26 +0300

    Align Zadarma migration with production registry

---
+++ supabase/migrations/20260823001603_rona_voice_zadarma_kg_v1_3.sql
@@ -0,0 +1,10 @@
+begin;
+
+alter table portal_private.voice_gateway_control
+  drop constraint if exists voice_gateway_control_provider_mode_check;
+
+alter table portal_private.voice_gateway_control
+  add constraint voice_gateway_control_provider_mode_check
+  check (provider_mode in ('UNCONFIGURED','PLIVO_SIP_BRIDGE','TWILIO_SIP_BRIDGE','ZADARMA_KG_SIP_BRIDGE'));
+
+commit;
