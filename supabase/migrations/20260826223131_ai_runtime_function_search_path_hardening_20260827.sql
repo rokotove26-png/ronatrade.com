2b7b00bf98e73c7480c2edb5b9ba739e88ff1890
rokotove26-png <rokotove26@gmail.com>
2026-08-27 01:31:59 +0300

    Mirror AI runtime function search_path hardening migration

---
+++ supabase/migrations/20260826223131_ai_runtime_function_search_path_hardening_20260827.sql
@@ -0,0 +1,3 @@
+alter function portal_private.ai_runtime_deadline(text,timestamptz) set search_path to pg_catalog,portal_private;
+alter function portal_private.ai_runtime_priority(text) set search_path to pg_catalog,portal_private;
+alter function portal_private.ai_executor_role_scope(portal_private.ai_business_role_enum,text) set search_path to pg_catalog,portal_private;
