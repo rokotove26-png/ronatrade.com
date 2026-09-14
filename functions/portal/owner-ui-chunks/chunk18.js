49dcb38d83f47248280d2ed82e9c46f7579c32f0
rokotove26-png <rokotove26@gmail.com>
2026-09-14 23:40:43 +0300

    Admin Operations: compose 120% scale visual layer

--- functions/portal/owner-ui-chunks/chunk18.js
+++ functions/portal/owner-ui-chunks/chunk18.js
@@ -8,5 +8,6 @@ import adminOperationsFlightdeckTitleDedupeRuntime from '../main-ui/admin-operat
 import adminOperationsFlightdeckReadabilityRuntime from '../main-ui/admin-operations-flightdeck-readability-v1.js';
 import adminOperationsFlightdeckLayoutRuntime from '../main-ui/admin-operations-flightdeck-layout-v1.js';
 import adminOperationsFlightdeckPackedRuntime from '../main-ui/admin-operations-flightdeck-packed-v1.js';
+import adminOperationsFlightdeckScale120Runtime from '../main-ui/admin-operations-flightdeck-scale-120-v1.js';
 
-export default baseRuntime + adminDealsPremiumRuntime + adminDealsSemanticButtonsRuntime + adminDealsSemanticCorrectionsRuntime + adminDealPassportPremiumRuntime + adminOperationsPremiumNightRuntime + adminOperationsFlightdeckTitleDedupeRuntime + adminOperationsFlightdeckReadabilityRuntime + adminOperationsFlightdeckLayoutRuntime + adminOperationsFlightdeckPackedRuntime;
+export default baseRuntime + adminDealsPremiumRuntime + adminDealsSemanticButtonsRuntime + adminDealsSemanticCorrectionsRuntime + adminDealPassportPremiumRuntime + adminOperationsPremiumNightRuntime + adminOperationsFlightdeckTitleDedupeRuntime + adminOperationsFlightdeckReadabilityRuntime + adminOperationsFlightdeckLayoutRuntime + adminOperationsFlightdeckPackedRuntime + adminOperationsFlightdeckScale120Runtime;
