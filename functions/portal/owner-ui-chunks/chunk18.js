af896f51d3c4482f952fe8ebede34b8405edd2c8
rokotove26-png <rokotove26@gmail.com>
2026-09-15 04:20:18 +0300

    style(admin): load compact operations override

--- functions/portal/owner-ui-chunks/chunk18.js
+++ functions/portal/owner-ui-chunks/chunk18.js
@@ -9,5 +9,6 @@ import adminOperationsFlightdeckReadabilityRuntime from '../main-ui/admin-operat
 import adminOperationsFlightdeckLayoutRuntime from '../main-ui/admin-operations-flightdeck-layout-v1.js';
 import adminOperationsFlightdeckPackedRuntime from '../main-ui/admin-operations-flightdeck-packed-v1.js';
 import adminOperationsFlightdeckScale120Runtime from '../main-ui/admin-operations-flightdeck-scale-120-v1.js';
+import adminOperationsFlightdeckCompactV2Runtime from '../main-ui/admin-operations-flightdeck-compact-v2.js';
 
-export default baseRuntime + adminDealsPremiumRuntime + adminDealsSemanticButtonsRuntime + adminDealsSemanticCorrectionsRuntime + adminDealPassportPremiumRuntime + adminOperationsPremiumNightRuntime + adminOperationsFlightdeckTitleDedupeRuntime + adminOperationsFlightdeckReadabilityRuntime + adminOperationsFlightdeckLayoutRuntime + adminOperationsFlightdeckPackedRuntime + adminOperationsFlightdeckScale120Runtime;
+export default baseRuntime + adminDealsPremiumRuntime + adminDealsSemanticButtonsRuntime + adminDealsSemanticCorrectionsRuntime + adminDealPassportPremiumRuntime + adminOperationsPremiumNightRuntime + adminOperationsFlightdeckTitleDedupeRuntime + adminOperationsFlightdeckReadabilityRuntime + adminOperationsFlightdeckLayoutRuntime + adminOperationsFlightdeckPackedRuntime + adminOperationsFlightdeckScale120Runtime + adminOperationsFlightdeckCompactV2Runtime;
