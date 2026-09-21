acfc0b2e0dd34b0d34bee5cec932beccd4033c7a
rokotove26-png <rokotove26@gmail.com>
2026-09-22 00:59:23 +0300

    backup: snapshot rona-owner-acceptance/index.ts

---
+++ emergency_backups/2026-09-22/supabase_edge/rona-owner-acceptance/index.ts
@@ -0,0 +1,1 @@
+import "https://raw.githubusercontent.com/rokotove26-png/ronatrade.com/94ddcae25187ce5df5ab81966a52881fe41bc92a/supabase/functions/rona-owner-acceptance/application-owner-boundary-bootstrap-v3.ts";
